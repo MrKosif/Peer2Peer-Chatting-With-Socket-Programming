9c3c17c7f31b898492ab9093fbc4cc81b90af136
MrKosif <keremksf@gmail.com>
2022-01-11 21:53:45 +0300

    BITTI

--- client.py
+++ client.py
@@ -5,39 +5,43 @@ import threading
 import time
 import datetime
 
+# TCP Socket Connection to the registry server
 ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ClientSocket.connect(('192.168.1.2', 5556))
 
-udp_socket = 4446
+# Setting up the UDP socket for comminication.
+udp_socket = 4447
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(("", udp_socket))
 
+# Header length for begging of the pickled 
+# datas to receive the right amount of data.
 HEADER_LENGTH = 10
 global busy
 busy = False
 online = False
 
-## second olarak variable gir
-
+# This variables are configured when a peer sends chat request.
 my_username = ""
 friends_username = ""
 
 job = threading.Thread()
-
 print('Waiting for connection')
-
 ##################  RECEIVE SEND LOG ##################
+# This function is for storing messages on log files.
+# It also contains date and time variables for proper logging.
 def add_to_log(info, username=my_username):
     time = str(datetime.datetime.now()).split(" ")[1].split(".")[0]
     date = str(datetime.datetime.now()).split(" ")[0]
     with open(f"{my_username}_user_log.txt", "a+") as file:
         file.write(f"{date} {time} {username}: {info}\n")
 
+# This function is for pickling objects and created for ease of use.
 def format_message(data):
     pickled_data = pickle.dumps(data)
     return bytes(f'{len(pickled_data):<{HEADER_LENGTH}}', 'utf-8') + pickled_data
 
+# This function is for receiving objects from server. And contains load oeperations.
 def receive_object(client_socket):
     message_header = client_socket.recv(HEADER_LENGTH)
     if not len(message_header):
@@ -49,6 +53,7 @@ def receive_object(client_socket):
 ################################################
 
 ##################  REGISTER  ##################
+# Register user is used for registering client to registry system.
 def register_user(client_socket):
     print("##### Registration ##### ")
     username = input("Username: ")
@@ -56,6 +61,8 @@ def register_user(client_socket):
     if username == "" or password == "":
         print("Please Enter valid inputs!")
         return
+    # Here we sent command along with the required 
+    # datas and used format message to pickle the data
     register_credentials = {"command": "REGISTER", "username": username, "password": password, "port": udp_socket}
     client_socket.send(format_message(register_credentials))
     data = client_socket.recv(1024).decode("utf-8")
@@ -67,11 +74,13 @@ def register_user(client_socket):
 #############################################
 
 ##################  LOGIN  ##################
+# This function is for login to registry system.
 def login_user(client_socket):
     print("##### Login ##### ")
     username = input("Username: ")
     password = input("Password: ")
     print("---------------------------")
+    # dictionary format of the dictionary data.
     register_credentials = {"command": "LOGIN", "username": username, "password": password}
     client_socket.send(format_message(register_credentials))
     user_data = (client_socket.recv(1024)).decode("utf-8")
@@ -79,18 +88,23 @@ def login_user(client_socket):
     add_to_log(user_data, username)
 
     if user_data == "CONFIRMED":
+        # If registry sends CONFIRMED message then login menu function executes.
         print("User successfully logined!")
+        # Online is used for HELLO signals
         global online
         online = True
+        # After user logined it starts to sending HELLO messages.
         signal = threading.Thread(target=send_signal, args=(client_socket, username))
         signal.start()
         logined_menu(username, client_socket)
 
     elif user_data == "DECLINED":
+        # Sends user to the start menu if declines.
         print("Incorrect username of password")
         return
 
 def logined_menu(username, client_socket):
+    # This menu pops up when user logined to the system.
     global my_username
     my_username = username
     while True:
@@ -112,6 +126,8 @@ def logined_menu(username, client_socket):
                 break
 
             elif choice == "2":
+                # Sends LOGOUT command to the registry and making 
+                # itself offline to stop sending HELLO messages.
                 global online
                 client_socket.send(format_message({"command": "LOGOUT", "username": username}))
                 add_to_log("LOGOUT", username)
@@ -128,21 +144,25 @@ def logined_menu(username, client_socket):
 
 ###############  SEARCH  #######################
 def handle_search(username, client_socket):
+    # Sends registry SEARCH command along with the requested username.
     search_request = {"command": "SEARCH", "username": username}
     client_socket.send(format_message(search_request))
     add_to_log("SEARCH", username)
     user_data = receive_object(client_socket)
     add_to_log(user_data[0], username)
     if user_data[0] == "NOT FOUND":
+        # If not found informs the user.
         print("User is not found!")
         print()
         return
+    # friends username is set here and used later on the chatting system.
     global friends_username
     friends_username = username
     IP = user_data[0]
     PORT = user_data[1]
     print()
     while True:
+        # This parts purpose is asking the user if it wants to chat with searched peer.
         print("User found!")
         print("---------------------------")
         print(f"{friends_username} | {IP} | {PORT}")
@@ -151,13 +171,14 @@ def handle_search(username, client_socket):
         while True:
             ans = input(">> ")
             if ans == "yes":
+                # Sending CHAT REQUEST command along with username.
                 server_socket.sendto(str.encode("CHAT REQUEST|" + my_username), (IP, PORT))
                 add_to_log("CHAT REQUEST", username)
                 return
             elif ans == "no":
                 return
             else:
-                print("Please provide valid input!!!!!!!!!!!!!!!!!!!!!")
+                print("Please provide valid input!")
 ############################################################
 
 ###################### CHATT PARTT #########################
@@ -165,16 +186,20 @@ def chatting(ip, port):
     global my_username
     while True:
         message = input()
+        # This part is explained in the raport shortly its using for clean chatting in terminal.
         print ('\033[1A' + my_username + ": " + message + '\033[K')
         if message == "exit":
+            # Informing the other peer that his friend disconnected from chat.
             server_socket.sendto(str.encode(f"User {my_username} disconnected from chat!"), (ip, port))
             add_to_log(f"User {my_username} disconnected from chat!", my_username)
             return
+        # Sending the message using UDP.
         server_socket.sendto(str.encode(message), (ip, port))
         add_to_log("CHAT REQUEST", my_username)
 
 
 def chat_request(ip, port):
+    # This function pops when a peer sends a chatting request.
     print(f"User {friends_username} want to chat with you.")
     print("Whould you like to accept? (yes/no)")
     print("Pres Enter to answer!")
@@ -182,6 +207,7 @@ def chat_request(ip, port):
     while True:
         answer = input(">> ")
         if answer == "yes":
+            # If answer yes it sends OK
             server_socket.sendto(str.encode("OK"),(ip, port)) 
             add_to_log("OK", my_username)
             print("Chatting is starting...")
@@ -189,6 +215,7 @@ def chat_request(ip, port):
             chatting(ip, port)
             return
         elif answer == "no":
+            # If no it sends REJECT
             server_socket.sendto(str.encode("REJECT"),(ip, port)) 
             add_to_log("REJECT", my_username)
             return
@@ -199,37 +226,37 @@ def chat_request(ip, port):
 ################  LISTENING PART  #################
 def handle_request(command, ip, port):
     global friends_username
-    if command[:12] == "CHAT REQUEST":
+    if command[:12] == "CHAT REQUEST": #getting chat request signal form the command 
         friends_username = command.split("|")[1]
-        add_to_log("CHAT REQUEST", friends_username)
+        add_to_log("CHAT REQUEST", friends_username) #adding chat request signal inforamtion in to log file 
         global busy
         if busy == True:
-            server_socket.sendto(str.encode("BUSY"),(ip, port)) 
+            server_socket.sendto(str.encode("BUSY"),(ip, port))  #sending a busy signal to the server.py
             return
         
         busy = True
-        chat_request(ip, port)
+        chat_request(ip, port) 
         busy = False
         return
 
-    add_to_log(command, friends_username)
+    add_to_log(command, friends_username) #adding chat information inside log 
     if command == "OK":
-        print("Pres Enter to start chatting")
+        print("Pres Enter to start chatting")  #getting confirmation from the user to start a chat
         busy = True
-        chatting(ip, port)
+        chatting(ip, port) # sending ip and port adress of the user 
         busy = False
         return
 
-    elif command == "REJECT":
+    elif command == "REJECT": # reject signal if the user reject of a chat request 
         print(f"User {ip} rejected the request!")
         return
 
-    elif command == "BUSY":
+    elif command == "BUSY": # if user us already chatting with someone else this signal is sent
         print(f"User {ip} is chatting with someone else!")
         return
 
     else:
-        print(f"{friends_username}: {command}")
+        print(f"{friends_username}: {command}") # printing the other side users username in terminal
 
 
 def listen_socket():
@@ -238,16 +265,18 @@ def listen_socket():
     while True:
         msg = server_socket.recvfrom(1024)
         command = msg[0].decode("utf-8")
-        ip = msg[1][0]
-        port = msg[1][1]
+        ip = msg[1][0] #ip address of user
+        port = msg[1][1] #port address of user
         #start_new_thread(handle_request, (command, ip, port))
         global job
-        job = threading.Thread(target=handle_request, args=(command, ip, port))
+        job = threading.Thread(target=handle_request, args=(command, ip, port)) 
+        #starting a thread for the requests
         job.start()
 ##################################################
 
 ##################  START MENU  ##################
 def start_menu(client_socket):
+    #main menu while loop 
     while True:
         print()
         print("---------------------------")
@@ -258,14 +287,14 @@ def start_menu(client_socket):
         print("---------------------------")
 
         while True:
-            choice = input(">> ")
+            choice = input(">> ") #getting input from user
             print()
             if choice == "1":
-                register_user(client_socket)
+                register_user(client_socket) # option 1 is for the registering user
                 break
 
             elif choice== "2":
-                login_user(client_socket)
+                login_user(client_socket) # option 2 is for the login user
                 break
 
             else:
@@ -278,21 +307,21 @@ def send_signal(client_socket, username):
     while True:
         if online == True:
             while True:
-                client_socket.send(format_message({"command": "HELLO", "username": username}))
+                client_socket.send(format_message({"command": "HELLO", "username": username})) #sending Hello signal to the server
                 #add_to_log("HELLO", my_username)
-                time.sleep(6)
+                time.sleep(6) #6 second waiting time for the hello signal
                 break
         else:
             return
 #######################################################
 
 def main():
-    li = threading.Thread(target=listen_socket)
-    men = threading.Thread(target=start_menu, args=(ClientSocket,))
-    li.start()
-    men.start()
+    li = threading.Thread(target=listen_socket) #defining threads for the listen socket
+    men = threading.Thread(target=start_menu, args=(ClientSocket,)) #defining threads for the start menu
+    li.start() #starting threads
+    men.start() #starting threads
     li.join()
     men.join()
-    ClientSocket.close()
+    ClientSocket.close() #finisihing socket process
 
 main()
--- server.py
+++ server.py
@@ -6,52 +6,57 @@ import time
 import datetime
 import threading
 
-ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #setting TCP connection inside server
 #ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ServerSocket.bind(("", 5556))
 
+
+#dictionarys for the storng data 
 ThreadCount = 0
-HEADER_LENGTH = 10
+HEADER_LENGTH = 10 #header lenght size
 login_credentials = {}
 client_addresses = {}
 socket_addresses = {}
-online_users = {}
+online_users = {} #Online users information dictionary
 
 print('Waitiing for a Connection..')
-ServerSocket.listen(5)
+ServerSocket.listen(5) #support max 5 people conenction, can be chnage in later
 
+#creating log file necesarry history and date information 
 def add_to_log(info):
     time = str(datetime.datetime.now()).split(" ")[1].split(".")[0]
     date = date = str(datetime.datetime.now()).split(" ")[0]
     with open("server_log.txt", "a+") as file:
-        file.write(f"{date} {time}: {info}\n")
+        file.write(f"{date} {time}: {info}\n") # writing inside of file with date
 
+#formatting messages 
 def format_message(data):
-    pickled_data = pickle.dumps(data)
+    pickled_data = pickle.dumps(data) #paersing pickle data  with utf 8 format
     return bytes(f'{len(pickled_data):<{HEADER_LENGTH}}', 'utf-8') + pickled_data
 
 
+#getting data from clients 
 def receive_object(client_socket):
     message_header = client_socket.recv(HEADER_LENGTH)
     if not len(message_header):
         return False
 
-    client_data = client_socket.recv(int(message_header.strip()))
-    unpickled_data = pickle.loads(client_data)
+    client_data = client_socket.recv(int(message_header.strip())) #receiving a message from client
+    unpickled_data = pickle.loads(client_data) #unpacking data
     return unpickled_data
 
-#################### REGISTER #####################
+#################### REGISTER #####################
 def register_client(user_data, client_socket):
     username = user_data["username"]
     if username not in login_credentials:
-        login_credentials[username] = user_data["password"]
-        socket_addresses[client_socket].append(user_data["port"])
+        login_credentials[username] = user_data["password"] ## setting password to user
+        socket_addresses[client_socket].append(user_data["port"]) # appending inside dictonary 
         client_addresses[username] = socket_addresses[client_socket]
         client_socket.sendall(str.encode("User successfully created!"))
-        add_to_log("User successfully created!")
+        add_to_log("User successfully created!") #sending into log file 
 
-    elif username in login_credentials:
-        client_socket.sendall(str.encode("This username already in use!"))
+    elif username in login_credentials: #checking if user is aldready exist
+        client_socket.sendall(str.encode("This username already in use!")) #sending exist data information to user
         add_to_log("This username already in use!")
 
 
@@ -59,14 +64,14 @@ def register_client(user_data, client_socket):
 def login_client(user_data, client_socket):
     username = user_data["username"]
     password = user_data["password"]
-    if username in login_credentials:
-        if password == login_credentials[username]:
+    if username in login_credentials: #login to user 
+        if password == login_credentials[username]: #checking login information of the user
             online_users[username] = 0
-            client_socket.sendall(str.encode("CONFIRMED"))
-            add_to_log("CONFIRMED")
+            client_socket.sendall(str.encode("CONFIRMED")) #sending info mesage to user
+            add_to_log("CONFIRMED") #adding confirmation log message to log file 
             return
 
-    client_socket.sendall(str.encode("DECLINED"))
+    client_socket.sendall(str.encode("DECLINED")) #sending declined message to log file 
     add_to_log("DECLINED")
 
 
@@ -75,15 +80,15 @@ def login_client(user_data, client_socket):
 def search_users(user_data, client_socket):
     # This only checks the user list but it should also check if the user online or not
     for username in client_addresses:
-        if username == user_data["username"]:
+        if username == user_data["username"]: #seraching user with their username
             if username in online_users:
                 #packet = {"command": "FOUND", "username": username}
                 client_socket.send(format_message(client_addresses[username]))
                 add_to_log(client_addresses[username])
                 return
 
-    client_socket.sendall(format_message(["NOT FOUND"]))
-    add_to_log("NOT FOUND")
+    client_socket.sendall(format_message(["NOT FOUND"]))# sending message to client if user not found
+    add_to_log("NOT FOUND")#adding inside log dile not found signal
 
 
 def threaded_client(connection):
@@ -91,43 +96,49 @@ def threaded_client(connection):
     while True:
         data = receive_object(connection)
         try:
-            add_to_log(str(data["command"]))
+            add_to_log(str(data["command"])) # getting signals 
         except:
             pass
         if not data or data==False:
             break
 
-        elif data["command"] == "HELLO":
+        elif data["command"] == "HELLO": # hello signal for the maintaining the connection
             online_users[data["username"]] = 0
 
-        elif data["command"] == "REGISTER":
+        elif data["command"] == "REGISTER":# register signal for registering a new client
             register_client(data, connection)
 
-        elif data["command"] == "LOGIN":
+        elif data["command"] == "LOGIN":# login signal for loggin a user into system
             login_client(data, connection)
 
-        elif data["command"] == "SEARCH":
+        elif data["command"] == "SEARCH": # search signal for searching a new user to chat
             search_users(data, connection)
 
-        elif data["command"] == "LOGOUT":
+        elif data["command"] == "LOGOUT":# logout signal forthe terminating all the process for the current user
             username = data["username"]
             print(online_users)
             del online_users[username]
             print(online_users)
-            print(f"{username} is logged out!")
-            add_to_log(f"{username} is logged out!")
+            print(f"{username} is logged out!") #printing logout message into the CLI
+            add_to_log(f"{username} is logged out!") #sending logout information to the log file
 
     connection.close()
 
 def connection_guard():
+    # This function is for checking HELLO messages and setting users 
+    # offline if they don't send HELLO for 20 seconds.
+    # I set up the system like every user has 0 points in the beggining.
+    # every second they gain 1 point and every second loop checks if any user
+    # reaches 20 points. if it does users condition sets to offline.
+    # if user sends HELLO to the system points reset to 0.
     naughty_list = []
     while True:
         print(online_users)
         for username in online_users:
-            if online_users[username] == 20:
+            if online_users[username] == 20: #counting 20 sec for the logging out user
                 naughty_list.append(username)
-                print(f"{username}'s connection is terminated!")
-                add_to_log(f"{username}'s connection is terminated!")
+                print(f"{username}'s connection is terminated!")#printing terminated message to the terminal
+                add_to_log(f"{username}'s connection is terminated!") #adding terminated information to the log file 
                 continue
             online_users[username] += 1
         
@@ -137,16 +148,16 @@ def connection_guard():
         time.sleep(1)
 
 def main():
-    guard = threading.Thread(target=connection_guard)
-    guard.start()
+    guard = threading.Thread(target=connection_guard) #defining thread
+    guard.start() #starting athread
 
     while True:
-        client_socket, address = ServerSocket.accept()
+        client_socket, address = ServerSocket.accept() #accepting socket informations
         socket_addresses[client_socket] = [address[0]]
-        print('Connected to: ' + address[0] + ':' + str(address[1]))
-        add_to_log('Connected to: ' + address[0] + ':' + str(address[1]))
+        print('Connected to: ' + address[0] + ':' + str(address[1])) #viewing the connected users ip addresses 
+        add_to_log('Connected to: ' + address[0] + ':' + str(address[1])) #adding the connected users ip addresses into log file
         #start_new_thread(threaded_client, (client_socket, ))
-        client = threading.Thread(target=threaded_client, args=((client_socket,)))
+        client = threading.Thread(target=threaded_client, args=((client_socket,)))#starting a thread with threaded_client
         client.start()
 
 main()
